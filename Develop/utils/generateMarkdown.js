441b08a010ee6a9c7f26b407d5bf5fe67d0cdf16
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-04-18 10:13:38 -0500

    editing the markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,15 +18,107 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license===licenseArr[0]) {
+    return "https://opensource.org/licenses/MIT";
+  }else if (license===licenseArr[1]) {
+    return "https://opensource.org/licenses/Apache-2.0";  
+  }else if (license===licenseArr[2]) {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  }else if (license===licenseArr[3]) {
+    return "https://opensource.org/licenses/BSD-3-Clause";
+  }else if (license===licenseArr[4]) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license===licenseArr[0]) {
+    return "This project is licensed under the MIT license.";
+  }else if (license===licenseArr[1]) {
+    return "This project is licensed under the Apache 2.0 license.";  
+  }else if (license===licenseArr[2]) {
+    return "This project is licensed under the GPL 3.0 license.";
+  }else if (license===licenseArr[3]) {
+    return "This project is licensed under the BSD 3 license.";
+  }else if (license===licenseArr[4]) {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+  *[license](#license)
+
+  *[title](#title)
+
+  *[usage](#usage)
+
+  *[description](#description)
+
+  *[installation](#installation)
+
+  *[tests](#tests)
+
+  *[contributing](#contributing)
+
+  *[challenge](#challenge)
+
+  *[github](#github)
+
+  *[email](#email)
+
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
+
+  ## Title
+  ${data.title}
+
+
+  ## Usage
+  ${data.usage}
+
+
+  ## Description
+  ${data.description}
+
+
+  ## Installation
+  ${data.installation}
+
+
+  ## Tests
+  ${data.tests}
+
+
+  ## Contributing
+  [Contributor Covenant](https://www.contributor-covenant.org/)
+  ${data.contributing}
+
+
+  ## Challenge
+  ${data.challenge}
+
+
+  ## GitHub
+  ${data.github}
+
+
+  ## Email
+  ${data.email}
+
+  
+  ## Questions
+  If you have any questions, please contact me at [Jason Weaver](Jamaweaver@yahoo.com) or visit my GitHub page at [Jweaver74](https://github.com/Jweaver74)
 
 `;
 }
